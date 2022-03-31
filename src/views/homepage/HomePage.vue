f719124504f5a23ca92bb710b66a2af4275e19a1
Jamcy123 <1219502823@qq.com>
2022-03-31 12:34:15 +0800

    TabControl

--- src/views/homepage/HomePage.vue
+++ src/views/homepage/HomePage.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:tab-control="http://www.w3.org/1999/xlink">
   <div id="homepage">
     <nav-bar class="home-nav">
       <template v-slot:center><div>首页</div></template>
@@ -6,20 +6,38 @@
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature/>
+    <tab-control :titles="['流行', '新款', '精选']" />
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import tabControl from "components/content/tabControl/TabControl"
+
 import HomeSwiper from "./childcomps/HomeSwiper";
-import {getHomeMultidata} from "network/homepage";
 import recommendView from "views/homepage/childcomps/RecommendView";
 import feature from "views/homepage/childcomps/feature";
+
+import { getHomeMultidata, getHomeGoods } from "network/homepage";
 
 export default {
   name: 'HomePage',
   components: {
     NavBar,
+    tabControl,
     HomeSwiper,
     recommendView,
     feature
@@ -27,15 +45,37 @@ export default {
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      }
     }
   },
   created() {
     // 1. 请求多个数据
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+    this.getHomeMultidata()
+
+    // 2. 请求商品数据
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      })
+    }
   }
 }
 </script>
